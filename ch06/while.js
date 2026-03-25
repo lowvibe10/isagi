d3bf7acf6439cab8c48988464c9371ed55ba1c72
lowvibe10 <itaehui781@gmail.com>
2026-03-25 10:24:54 +0900

    cho6

---
+++ ch06/while.js
@@ -0,0 +1,11 @@
+// while (조건식) {
+//     조건식이 참일 때 실행할 구문
+// }
+
+// 1월 ~ 12월까지 출력
+let month = 1;
+while (month <= 12) {
+    console.log(month + "월");
+    month ++; // month = mnth + 1;
+    // 조건식이 참일 때 실행할 구문
+}

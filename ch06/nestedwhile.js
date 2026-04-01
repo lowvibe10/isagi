7b2322b53b371dadbab3e7fdecd0f5c845e457c7
lowvibe10 <itaehui781@gmail.com>
2026-04-01 14:54:25 +0900

    조건문 반복문 외

---
+++ ch06/nestedwhile.js
@@ -0,0 +1,16 @@
+// 2중 for 문을 사용해 2단 ~ 9단 구구단을 출력하세요.
+let dan = 2;
+let num = 1;
+
+whille (dan <= 9) {
+    num = 1;
+    console.log(`${dan}단`);
+    while (num <= 9) {
+        console.log(`${dan} x ${num} = ${dan * num}`);
+        num++;
+    }
+    dan++;
+    console.log(`\n`);
+}
+
+// label을 사용해 
